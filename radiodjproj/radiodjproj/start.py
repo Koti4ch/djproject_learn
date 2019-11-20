4309b07e03a6d04719c3a3a3e770d0dad0dab6d9
Natalius <Koti4Matroskin@gmail.com>
2019-11-20 19:31:28 +0300

    added map and messages

--- radiodjproj/radiodjproj/start.py
+++ radiodjproj/radiodjproj/start.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib import messages
 from account.forms import RegistrationUserForm
 
 
 
 def startpage(request):
+    messages.success(request, 'test SUCCESS message!')
+    messages.debug(request, 'test DEBUG message!')
+    messages.warning(request, 'test WARNING message!')
+    messages.error(request, 'test ERROR message!')
+    messages.info(request, 'test INFO message!')
     return render(request, 'eventgen/event/all_active.html')
