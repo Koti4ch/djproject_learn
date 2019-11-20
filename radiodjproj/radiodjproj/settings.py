98cc0491f2bd3867669fdfb915bb5884114b337c
Natalius <Koti4Matroskin@gmail.com>
2019-11-20 18:28:58 +0300

    modify base template situated

--- radiodjproj/radiodjproj/settings.py
+++ radiodjproj/radiodjproj/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_TEMPLATES_DIR = BASE_DIR + '/templates/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -71,7 +72,7 @@ ROOT_URLCONF = 'radiodjproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
